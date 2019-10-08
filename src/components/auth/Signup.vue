178302e79ee72709f7ef4504b9e9a8fd07dc084e
Jamo <jamo@Jamos-MBP.fios-router.home>
2019-10-08 12:26:54 -0400

    cleaned and added new code

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -41,48 +41,39 @@ export default {
     },
     methods: {
         signup(){
-            // HERM: this if checks that you have alias, email, password
              if(this.alias && this.email && this.password) {
                  this.slug = slugify(this.alias, {
                      replacement: '-',
                      remove: /[$*_+~.()'"!\-:@]/g,
                      lower: true
                  })
-                //  checks and sees if there is an alias on record
                  let ref = db.collection('users').doc(this.slug)
                  ref.get().then( doc => {
                      if(doc.exists){
-                         // H: We complain that the alias already exists and do nothing
                          this.feedback = 'This alias already exists'
                      } else {
-                         // H: Here we know that alias, email, and password exist
-                         // H: We also know that the slug is good
-                         // H: We also know that the alias is free to use
-                         // H: THIS is a good place to put the auth() code. 
                          this.feedback = 'This alias is free to use'
 
                          firebase.auth().createUserWithEmailAndPassword(this.email , this.password)
-                        .catch(error => {
+                         .then(cred => {
+                                 console.log(cred.user);
+                             ref.set({
+                                 alias: this.alias,
+                                 geolocation: null,
+                                 user_id: cred.user.uid
+                             })
+                             //console.log(cred.user)
+                         }).then(() => {
+                             this.$router.push({  name: 'GMap'  })
+                         })
+                        .catch(err  => {
                             console.log(err)
                             this.feedback = err.message
                         })
                      }
                  })
                  console.log(this.slug)
-            // HERM: this is the else to the alias, email, password check
              } else {
-                 // H: anything in this else will ONLY run when:
-                 // H: alias or email or password is missing
-
-                 // H: This auth() call needs to happen IF (alias, email password) exists
-                 // H: So this auth() block of code is in the wrong place. 
-                //  firebase.auth().createUserWithEmailAndPassword(this.email , this.password)
-                //  .catch(error => {
-                //      console.log(err)
-                //      this.feedback = err.message
-                //  })
-
-                 // if alias or email or password are missing, you provide feedback to user
                  this.feedback = "You must enter all fields"
              }
         }
